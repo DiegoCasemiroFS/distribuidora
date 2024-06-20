cf310713270317540fb978f6f66026968939a341
Diego Casemiro <dcfs1998@gmail.com>
2024-06-19 21:11:24 -0300

    first commit

---
+++ src/main/java/br/com/DiegoCasemiroFS/demo/ApiDistribuidoraApplication.java
@@ -0,0 +1,13 @@
+package br.com.DiegoCasemiroFS.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDistribuidoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDistribuidoraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/DiegoCasemiroFS/demo/ApiDistribuidoraApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.DiegoCasemiroFS.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDistribuidoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
