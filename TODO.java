6ecfb686f1f0bb908567577333b7472032bae240
Diego Casemiro <dcfs1998@gmail.com>
2024-06-20 16:30:09 -0300

    mapeando as necessidades do projeto no TODO

---
+++ TODO.java
@@ -0,0 +1,7 @@
+- a pagina inicial deve apresentar varias colunas como se fosse no trelo;
+- as colunas devem ser PENDENTES, AUTORIZADOS, SEPARAÇÃO, CONFERENCIA, FATURAMENTO, TRANSPORTE;
+- cada coluna deve gerar um relatorio em excel/pdf;
+- nesse relatorio deve haver as seguintes informações: cliente, produto, quantidade, endereço, fornecedor, preço, marca, grupo;
+- deve existir um contador de dias sem pedido por cliente;
+- deve haver um grafico que compile todos os pedidos, notas, produtos por cliente, para facilitar um estudo de vendas;
+
