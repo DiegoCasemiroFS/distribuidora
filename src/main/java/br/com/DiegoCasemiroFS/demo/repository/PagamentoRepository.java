476de33ef52f6c68961a1fe2b4da1ca96ec5c89f
DiegoCasemiroFS <dcfs1998@gmail.com>
2024-07-05 14:59:51 -0300

    feat: remove PagamentoRepository

--- src/main/java/br/com/DiegoCasemiroFS/demo/repository/PagamentoRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.DiegoCasemiroFS.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PagamentoRepository extends JpaRepository<Pagamento, Long> {
-
-}
