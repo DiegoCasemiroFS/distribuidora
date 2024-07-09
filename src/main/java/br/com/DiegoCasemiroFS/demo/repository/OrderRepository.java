2ff611425f1b18c5b3a8b8aad28dcc8e465acc55
DiegoCasemiroFS <dcfs1998@gmail.com>
2024-07-09 16:33:35 -0300

    feat: add anotation repository & method findByIdFetchItens

--- src/main/java/br/com/DiegoCasemiroFS/demo/repository/OrderRepository.java
+++ src/main/java/br/com/DiegoCasemiroFS/demo/repository/OrderRepository.java
@@ -2,7 +2,16 @@ package br.com.DiegoCasemiroFS.demo.repository;
 
 import br.com.DiegoCasemiroFS.demo.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
+    @Query("SELECT o FROM Pedido o WHERE o.id = :orderId")
+    Optional<Order> findByIdFetchItens(@Param("orderId") Long orderId);
 }
+
