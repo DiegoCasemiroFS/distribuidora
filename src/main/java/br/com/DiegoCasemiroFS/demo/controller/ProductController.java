2de96dc056b4b70796db6c011f9a2f20c6bd5ec1
DiegoCasemiroFS <dcfs1998@gmail.com>
2024-07-09 16:36:35 -0300

    feat: update RequestMapping

--- src/main/java/br/com/DiegoCasemiroFS/demo/controller/ProductController.java
+++ src/main/java/br/com/DiegoCasemiroFS/demo/controller/ProductController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("v1/api/produto")
+@RequestMapping("v1/api/product")
 public class ProductController {
 
   @Autowired
