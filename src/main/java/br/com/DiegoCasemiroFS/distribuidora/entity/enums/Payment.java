aaa31c9f1991576b4f65b9bbc0e293bc6b282a9e
DiegoCasemiroFS <dcfs1998@gmail.com>
2024-07-10 11:56:41 -0300

    feat: delete class

--- src/main/java/br/com/DiegoCasemiroFS/distribuidora/entity/enums/Payment.java
+++
@@ -1,9 +0,0 @@
-package br.com.DiegoCasemiroFS.distribuidora.entity.enums;
-
-public enum Payment {
-
-    BOLETO,
-    PIX,
-    DEBITO,
-    CREDITO;
-}
--- src/main/java/br/com/DiegoCasemiroFS/distribuidora/repository/TransactionRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.DiegoCasemiroFS.distribuidora.repository;
-
-import br.com.DiegoCasemiroFS.distribuidora.entity.Transaction;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TransactionRepository extends JpaRepository<Transaction, Long> {
-
-}
