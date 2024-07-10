7cf3c1c4e3628bcef4b6c26174464a970470fb6e
DiegoCasemiroFS <dcfs1998@gmail.com>
2024-07-10 11:57:01 -0300

    feat: create class

---
+++ src/main/java/br/com/DiegoCasemiroFS/distribuidora/entity/dto/UpdateOrderDtoStatus.java
@@ -0,0 +1,9 @@
+package br.com.DiegoCasemiroFS.distribuidora.entity.dto;
+
+import lombok.Data;
+
+@Data
+public class UpdateOrderDtoStatus {
+
+    private String newStatus;
+}
---
+++ src/main/java/br/com/DiegoCasemiroFS/distribuidora/entity/enums/OrderStatus.java
@@ -0,0 +1,7 @@
+package br.com.DiegoCasemiroFS.distribuidora.entity.enums;
+
+public enum OrderStatus {
+
+    REALIZADO,
+    CANCELADO
+}
