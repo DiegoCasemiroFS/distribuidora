0ca60b01a692871db00fd1f622fe88fa32b4f5fb
DiegoCasemiroFS <dcfs1998@gmail.com>
2024-07-15 17:54:14 -0300

    feat: update imports

--- src/main/java/br/com/DiegoCasemiroFS/distribuidora/controller/ProductController.java
+++ src/main/java/br/com/DiegoCasemiroFS/distribuidora/controller/ProductController.java
@@ -2,16 +2,10 @@ package br.com.DiegoCasemiroFS.distribuidora.controller;
 
 import br.com.DiegoCasemiroFS.distribuidora.entity.Product;
 import br.com.DiegoCasemiroFS.distribuidora.service.ProductService;
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("v1/api/product")
