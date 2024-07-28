2dacd28e612184ec9252a35eb5e97dc414532856
dcfs1 <dcfs1998@gmail.com>
2024-07-28 19:59:33 -0300

    feat: create UserNotFoundException

---
+++ src/main/java/br/com/DiegoCasemiroFS/distribuidora/exception/UserNotFoundException.java
@@ -0,0 +1,8 @@
+package br.com.DiegoCasemiroFS.distribuidora.exception;
+
+public class UserNotFoundException extends RuntimeException {
+
+    public UserNotFoundException(){
+        super("Usuário não encontrado");
+    }
+}
