244e28db88e0cc971befe7a8139e767712ec7332
dcfs1 <dcfs1998@gmail.com>
2024-07-28 21:05:07 -0300

    feat: create ProductNotFoundException

---
+++ src/main/java/br/com/DiegoCasemiroFS/distribuidora/exception/ProductNotFoundException.java
@@ -0,0 +1,8 @@
+package br.com.DiegoCasemiroFS.distribuidora.exception;
+
+public class ProductNotFoundException extends RuntimeException {
+
+    public ProductNotFoundException(){
+        super("Produto não encontrado");
+    }
+}
