a6420b7f62e63955f2ad268afcaac783425b47d8
dcfs1 <dcfs1998@gmail.com>
2024-07-28 21:06:51 -0300

    feat: insert anotations

--- src/main/java/br/com/DiegoCasemiroFS/distribuidora/controller/UserController.java
+++ src/main/java/br/com/DiegoCasemiroFS/distribuidora/controller/UserController.java
@@ -4,10 +4,7 @@ import br.com.DiegoCasemiroFS.distribuidora.entity.User;
 import br.com.DiegoCasemiroFS.distribuidora.entity.dtos.UserRequestDto;
 import br.com.DiegoCasemiroFS.distribuidora.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,22 +15,27 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @GetMapping("/findUser")
     public User findUserById(@PathVariable Long id){
         return userService.findById(id);
     }
 
+    @GetMapping("/findAll")
     public List<User> findAllUsers(){
         return userService.findAll();
     }
 
+    @PostMapping("/createUser")
     public User createUser(@RequestBody User user){
         return userService.createUser(user);
     }
 
+    @PutMapping("/updateUser")
     public User updateUser(@PathVariable Long id, @RequestBody UserRequestDto userRequestDto){
         return userService.updateUser(id, userRequestDto);
     }
 
+    @DeleteMapping("/deleteUser")
     public void deleteUser(@PathVariable Long id){
         userService.deleteUser(id);
     }
