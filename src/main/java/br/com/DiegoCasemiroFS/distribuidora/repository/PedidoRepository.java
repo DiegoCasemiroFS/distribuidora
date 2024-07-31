0334e9e7fdaac48a3c0286257452cd39fd74f5a4
dcfs1 <dcfs1998@gmail.com>
2024-07-31 16:04:37 -0300

    feat: removendo import desnecessario

--- src/main/java/br/com/DiegoCasemiroFS/distribuidora/repository/PedidoRepository.java
+++ src/main/java/br/com/DiegoCasemiroFS/distribuidora/repository/PedidoRepository.java
@@ -4,8 +4,6 @@ import br.com.DiegoCasemiroFS.distribuidora.entity.Pedido;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
 
--- src/main/java/br/com/DiegoCasemiroFS/distribuidora/repository/ProdutoRepository.java
+++ src/main/java/br/com/DiegoCasemiroFS/distribuidora/repository/ProdutoRepository.java
@@ -4,8 +4,6 @@ import br.com.DiegoCasemiroFS.distribuidora.entity.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
 
