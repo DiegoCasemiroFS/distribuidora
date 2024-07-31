0a1a3802611ea1d1659852b6d3fc22608c0ec547
dcfs1 <dcfs1998@gmail.com>
2024-07-31 16:04:58 -0300

    feat: adicionando o tipo das requisicoes http

--- src/main/java/br/com/DiegoCasemiroFS/distribuidora/controller/PedidoController.java
+++ src/main/java/br/com/DiegoCasemiroFS/distribuidora/controller/PedidoController.java
@@ -4,10 +4,7 @@ import br.com.DiegoCasemiroFS.distribuidora.entity.Pedido;
 import br.com.DiegoCasemiroFS.distribuidora.entity.dtos.PedidoRequestDto;
 import br.com.DiegoCasemiroFS.distribuidora.service.PedidoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,22 +15,27 @@ public class PedidoController {
     @Autowired
     private PedidoService pedidoService;
 
+    @GetMapping("/findOrder")
     public Pedido findOrderById(@PathVariable Long id){
         return pedidoService.findById(id);
     }
 
+    @GetMapping("/findAll")
     public List<Pedido> findAllOrders(){
         return pedidoService.findAll();
     }
 
+    @PostMapping("/createOrder")
     public Pedido createOrder(@RequestBody PedidoRequestDto pedidoRequestDto){
         return pedidoService.createOrder(pedidoRequestDto);
     }
 
+    @PutMapping("/updateOrder")
     public Pedido updateOrder(@PathVariable Long id, @RequestBody PedidoRequestDto pedidoRequestDto){
         return pedidoService.updateOrder(id, pedidoRequestDto);
     }
 
+    @DeleteMapping("/deleteOrder")
     public void deleteOrderById(@PathVariable Long id){
         pedidoService.deleteOrder(id);
     }
