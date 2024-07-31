d6a596846b2aa855584c93b5abdeea28a06adad3
dcfs1 <dcfs1998@gmail.com>
2024-07-31 15:49:45 -0300

    feat: configurando a parte de autenticação

--- src/main/java/br/com/DiegoCasemiroFS/distribuidora/entity/Usuario.java
+++ src/main/java/br/com/DiegoCasemiroFS/distribuidora/entity/Usuario.java
@@ -3,15 +3,24 @@ package br.com.DiegoCasemiroFS.distribuidora.entity;
 import br.com.DiegoCasemiroFS.distribuidora.entity.enums.TipoUsuario;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 @Entity
 @Setter
+@Getter
 @AllArgsConstructor
 @NoArgsConstructor
 @Table(name = "usuario")
-public class Usuario {
+public class Usuario implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,10 +28,64 @@ public class Usuario {
 
     private String name;
 
+    private String email;
+
+    private String senha;
+
     private String address;
 
     private String phone;
 
+    private boolean admin;
+
     @Enumerated(EnumType.STRING)
     private TipoUsuario tipoUsuario;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority("ROLE_ADMIN"));
+    }
+
+    @Override
+    public String getPassword() {
+        return senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", senha='" + senha + '\'' +
+                ", address='" + address + '\'' +
+                ", phone='" + phone + '\'' +
+                ", tipoUsuario=" + tipoUsuario +
+                '}';
+    }
 }
