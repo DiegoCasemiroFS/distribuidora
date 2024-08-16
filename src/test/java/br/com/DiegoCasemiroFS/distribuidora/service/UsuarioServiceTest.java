8a65775be0fa5f87e040b999e10ddfe74f7a1c62
dcfs1 <dcfs1998@gmail.com>
2024-08-16 17:05:49 -0300

    feat: adicinando os metodos de teste na classe UsuarioServiceTest.java

---
+++ src/test/java/br/com/DiegoCasemiroFS/distribuidora/service/UsuarioServiceTest.java
@@ -0,0 +1,77 @@
+package br.com.DiegoCasemiroFS.distribuidora.service;
+
+import br.com.DiegoCasemiroFS.distribuidora.entity.Usuario;
+import br.com.DiegoCasemiroFS.distribuidora.exception.UserNotFoundException;
+import br.com.DiegoCasemiroFS.distribuidora.repository.UsuarioRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+
+@ExtendWith(MockitoExtension.class)
+class UsuarioServiceTest {
+
+    @Mock
+    private UsuarioRepository usuarioRepository;
+
+    @InjectMocks
+    private UsuarioService usuarioService;
+
+    private Usuario usuario;
+
+    @BeforeEach
+    void setUp(){
+        usuario = new Usuario();
+        usuario.setId(1L);
+    }
+
+    @Test
+    void findById_UserFound() {
+
+        when(usuarioRepository.findById(1L)).thenReturn(Optional.of(usuario));
+
+        Usuario foundUsuario = usuarioService.findById(1L);
+
+        assertNotNull(foundUsuario);
+        assertEquals(1L, foundUsuario.getId());
+        verify(usuarioRepository, times(1)).findById(1L);
+    }
+
+    @Test
+    void findById_UserNotFound(){
+
+        when(usuarioRepository.findById(1L)).thenReturn(Optional.empty());
+        assertThrows(UserNotFoundException.class, () -> usuarioService.findById(1L));
+        verify(usuarioRepository, times(1)).findById(1L);
+    }
+
+    @Test
+    void findAll_UsersFound(){
+
+        Usuario usuario1 = new Usuario();
+        usuario1.setId(1L);
+
+        Usuario usuario2 = new Usuario();
+        usuario2.setId(2L);
+
+        List<Usuario> usuarios = List.of(usuario1, usuario2);
+        when(usuarioRepository.findAll()).thenReturn(usuarios);
+        List<Usuario> foundUsuarios = usuarioService.findAll();
+
+        assertNotNull(foundUsuarios);
+        assertEquals(2, foundUsuarios.size());
+        assertEquals(1L, foundUsuarios.get(0).getId());
+        assertEquals(2L, foundUsuarios.get(1).getId());
+
+        verify(usuarioRepository, times(1)).findAll();
+    }
+}
