ae54f0578fc3a6635de4029a0a7f36a83ce85b9e
DiegoCasemiroFS <dcfs1998@gmail.com>
2024-08-30 16:59:40 -0300

    feat: include setter method

--- src/main/java/br/com/DiegoCasemiroFS/distribuidora/entity/dtos/ProductRequestDto.java
+++ src/main/java/br/com/DiegoCasemiroFS/distribuidora/entity/dtos/ProductRequestDto.java
@@ -3,10 +3,12 @@ package br.com.DiegoCasemiroFS.distribuidora.entity.dtos;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class ProductRequestDto {
