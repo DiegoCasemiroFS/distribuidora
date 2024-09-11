699bd5cd58b25b005f26385c11d51bf916c3c43f
DiegoCasemiroFS <dcfs1998@gmail.com>
2024-09-11 16:19:41 -0300

    feat: update class name

--- src/main/java/br/com/DiegoCasemiroFS/distribuidora/repository/UserRepository.java
+++ src/main/java/br/com/DiegoCasemiroFS/distribuidora/repository/UserRepository.java
@@ -1,13 +1,13 @@
 package br.com.DiegoCasemiroFS.distribuidora.repository;
 
-import br.com.DiegoCasemiroFS.distribuidora.entity.User;
+import br.com.DiegoCasemiroFS.distribuidora.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<Users, Long> {
 
-    Optional<User> findByEmail(String email);
+    Optional<Users> findByEmail(String email);
 }
