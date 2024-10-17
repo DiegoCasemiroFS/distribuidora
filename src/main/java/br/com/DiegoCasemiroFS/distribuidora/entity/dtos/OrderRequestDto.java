638d914139d65e106085c7651ec84e6bc602b1da
Diego Casemiro <dcfs1998@gmail.com>
2024-10-17 16:04:27 -0300

    feat: insert annotation @Setter

--- src/main/java/br/com/DiegoCasemiroFS/distribuidora/entity/dtos/OrderRequestDto.java
+++ src/main/java/br/com/DiegoCasemiroFS/distribuidora/entity/dtos/OrderRequestDto.java
@@ -1,8 +1,10 @@
 package br.com.DiegoCasemiroFS.distribuidora.entity.dtos;
 
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class OrderRequestDto {
 
     private Long userId;
