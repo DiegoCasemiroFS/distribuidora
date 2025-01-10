26e6f32d3e14555e5e42f703a902c5d5b8680884
Diego Casemiro <dcfs1998@gmail.com>
2025-01-10 10:35:28 -0300

    feat: create UserRepositoryTest methods

--- src/test/java/br/com/DiegoCasemiroFS/distribuidora/repository/UserRepositoryTest.java
+++ src/test/java/br/com/DiegoCasemiroFS/distribuidora/repository/UserRepositoryTest.java
@@ -1,7 +1,55 @@
 package br.com.DiegoCasemiroFS.distribuidora.repository;
 
-import static org.junit.jupiter.api.Assertions.*;
+import br.com.DiegoCasemiroFS.distribuidora.entity.Users;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
 class UserRepositoryTest {
 
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    private Users user;
+
+    @BeforeEach
+    void setUp() {
+        user = new Users();
+        user.setName("User Name");
+        entityManager.persistAndFlush(user);
+    }
+
+    @Test
+    void testFindById() {
+        Optional<Users> foundUser = userRepository.findById(user.getId());
+        assertThat(foundUser).isPresent();
+        assertThat(foundUser.get().getName()).isEqualTo(user.getName());
+    }
+
+    @Test
+    void testSaveUser() {
+        Users newUser = new Users();
+        newUser.setName("New User");
+        Users savedUser = userRepository.save(newUser);
+        assertThat(savedUser).isNotNull();
+        assertThat(savedUser.getId()).isNotNull();
+        assertThat(savedUser.getName()).isEqualTo("New User");
+    }
+
+    @Test
+    void testDeleteUser() {
+        userRepository.delete(user);
+        Optional<Users> deletedUser = userRepository.findById(user.getId());
+        assertThat(deletedUser).isNotPresent();
+    }
 }
