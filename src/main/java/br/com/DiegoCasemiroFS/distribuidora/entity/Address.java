efd0663c9f2d7a6f9a19214897fedb1da4b61b3c
Diego Casemiro <dcfs1998@gmail.com>
2025-01-22 20:36:21 -0300

    inserindo instancias

--- src/main/java/br/com/DiegoCasemiroFS/distribuidora/entity/Address.java
+++ src/main/java/br/com/DiegoCasemiroFS/distribuidora/entity/Address.java
@@ -1,4 +1,22 @@
 package br.com.DiegoCasemiroFS.distribuidora.entity;
 
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Embeddable
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Address {
+
+    private String street;
+    private String number;
+    private String neighborhood;
+    private String city;
+    private String state;
+    private String zipCode;
 }
