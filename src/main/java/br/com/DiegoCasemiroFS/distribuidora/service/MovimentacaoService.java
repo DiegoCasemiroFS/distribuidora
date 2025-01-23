2dffeb1d3b2b8960e783180533483aeaf3f43d15
Diego Casemiro <dcfs1998@gmail.com>
2025-01-23 18:20:34 -0300

    alterando metodos

---
+++ src/main/java/br/com/DiegoCasemiroFS/distribuidora/service/MovimentacaoService.java
@@ -0,0 +1,79 @@
+package br.com.DiegoCasemiroFS.distribuidora.service;
+
+import br.com.DiegoCasemiroFS.distribuidora.entity.Movimentacao;
+import br.com.DiegoCasemiroFS.distribuidora.entity.Produto;
+import br.com.DiegoCasemiroFS.distribuidora.entity.Usuario;
+import br.com.DiegoCasemiroFS.distribuidora.entity.dtos.MovimentacaoRequestDto;
+import br.com.DiegoCasemiroFS.distribuidora.entity.dtos.MovimentacaoResponseDto;
+import br.com.DiegoCasemiroFS.distribuidora.entity.enums.TipoUsuario;
+import br.com.DiegoCasemiroFS.distribuidora.exception.MovimentacaoNaoEncontradaException;
+import br.com.DiegoCasemiroFS.distribuidora.exception.ProdutoNaoEncontradoException;
+import br.com.DiegoCasemiroFS.distribuidora.exception.UsuarioNaoEncontradoException;
+import br.com.DiegoCasemiroFS.distribuidora.repository.MovimentacaoRepository;
+import br.com.DiegoCasemiroFS.distribuidora.repository.ProdutoRepository;
+import br.com.DiegoCasemiroFS.distribuidora.repository.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class MovimentacaoService {
+
+    private final MovimentacaoRepository movimentacaoRepository;
+    private final UsuarioRepository usuarioRepository;
+    private final ProdutoRepository produtoRepository;
+
+    public Movimentacao procuraPorId(Long id){
+        return movimentacaoRepository.findById(id)
+                .orElseThrow(MovimentacaoNaoEncontradaException::new);
+    }
+
+    public List<Movimentacao> listaTodas(){
+        return movimentacaoRepository.findAll();
+    }
+
+    public MovimentacaoResponseDto vendaCliente(MovimentacaoRequestDto movimentacaoRequestDto){
+        Usuario usuario = usuarioRepository.findById(movimentacaoRequestDto.getUsuarioId()).orElseThrow(UsuarioNaoEncontradoException::new);
+        Produto produto = produtoRepository.findById(movimentacaoRequestDto.getProdutoId()).orElseThrow(ProdutoNaoEncontradoException::new);
+
+        if (produto.getQuantidade() > movimentacaoRequestDto.getQuantidade()){
+            produto.setQuantidade(produto.getQuantidade() - movimentacaoRequestDto.getQuantidade());
+            produtoRepository.save(produto);
+
+            MovimentacaoResponseDto venda = new MovimentacaoResponseDto();
+            venda.setNomeUsuario(usuario.getNome());
+            venda.setNomeProduto(produto.getNome());
+            venda.setQuantidade(movimentacaoRequestDto.getQuantidade());
+            venda.setDataVenda(LocalDate.now());
+
+            return venda;
+        }
+        throw new IllegalArgumentException("Quantidade insuficiente em estoque");
+    }
+
+    public MovimentacaoResponseDto compraFornecedor(MovimentacaoRequestDto movimentacaoRequestDto){
+        Usuario usuario = usuarioRepository.findById(movimentacaoRequestDto.getUsuarioId()).orElseThrow(UsuarioNaoEncontradoException::new);
+        Produto produto = produtoRepository.findById(movimentacaoRequestDto.getProdutoId()).orElseThrow(ProdutoNaoEncontradoException::new);
+
+        if (usuario.getTipoUsuario() == TipoUsuario.FORNECEDOR){
+            produto.setQuantidade(produto.getQuantidade() + movimentacaoRequestDto.getQuantidade());
+            produtoRepository.save(produto);
+
+            MovimentacaoResponseDto compra = new MovimentacaoResponseDto();
+            compra.setNomeUsuario(usuario.getNome());
+            compra.setNomeProduto(produto.getNome());
+            compra.setQuantidade(movimentacaoRequestDto.getQuantidade());
+            compra.setDataVenda(LocalDate.now());
+
+            return compra;
+        }
+        throw new IllegalArgumentException("Usuário não é um fornecedor");
+    }
+
+    public void deletaMovimentacao(Long id){
+        movimentacaoRepository.deleteById(id);
+    }
+}
