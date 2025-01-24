ae1504306077890619b499b2e35447d49806829f
Diego Casemiro <dcfs1998@gmail.com>
2025-01-24 12:26:17 -0300

    inserindo instancia valorTotal

--- src/main/java/br/com/DiegoCasemiroFS/distribuidora/entity/dtos/MovimentacaoResponseDto.java
+++ src/main/java/br/com/DiegoCasemiroFS/distribuidora/entity/dtos/MovimentacaoResponseDto.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @Setter
@@ -14,5 +15,6 @@ public class MovimentacaoResponseDto {
     private String nomeUsuario;
     private String nomeProduto;
     private Integer quantidade;
+    private BigDecimal valorTotal;
     private LocalDate dataVenda;
 }
