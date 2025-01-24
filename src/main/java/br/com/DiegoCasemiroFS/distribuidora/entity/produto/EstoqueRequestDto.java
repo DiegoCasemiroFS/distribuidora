8d352a63d20d3bd8c898f6d128f554a0dea885a6
Diego Casemiro <dcfs1998@gmail.com>
2025-01-24 16:01:46 -0300

    mudando package

--- src/main/java/br/com/DiegoCasemiroFS/distribuidora/entity/produto/EstoqueRequestDto.java
+++ src/main/java/br/com/DiegoCasemiroFS/distribuidora/entity/produto/EstoqueRequestDto.java
@@ -1,11 +1,9 @@
-package br.com.DiegoCasemiroFS.distribuidora.entity.dtos;
+package br.com.DiegoCasemiroFS.distribuidora.entity.produto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import java.math.BigDecimal;
 
 @Getter
 @Setter
