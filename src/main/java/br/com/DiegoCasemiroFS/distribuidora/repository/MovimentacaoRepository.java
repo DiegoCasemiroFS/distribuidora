d2ebf5498d9695590843882dc7615352b9d06319
Diego Casemiro <dcfs1998@gmail.com>
2025-01-24 16:01:01 -0300

    mudando package

--- src/main/java/br/com/DiegoCasemiroFS/distribuidora/repository/MovimentacaoRepository.java
+++ src/main/java/br/com/DiegoCasemiroFS/distribuidora/repository/MovimentacaoRepository.java
@@ -1,6 +1,6 @@
 package br.com.DiegoCasemiroFS.distribuidora.repository;
 
-import br.com.DiegoCasemiroFS.distribuidora.entity.Movimentacao;
+import br.com.DiegoCasemiroFS.distribuidora.entity.movimentacao.Movimentacao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
