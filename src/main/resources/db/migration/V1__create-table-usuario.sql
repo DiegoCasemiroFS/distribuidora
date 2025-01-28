c3a532af54645f57fa08e3fbc17c02bc96c67139
Diego Casemiro <dcfs1998@gmail.com>
2025-01-28 14:50:12 -0300

    criando tabela

---
+++ src/main/resources/db/migration/V1__create-table-usuario.sql
@@ -0,0 +1,16 @@
+CREATE TABLE usuario (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    senha VARCHAR(255) NOT NULL,
+    telefone VARCHAR(15) NOT NULL,
+    rua VARCHAR(255),
+    numero INTEGER,
+    complemento VARCHAR(255),
+    bairro VARCHAR(255),
+    cidade VARCHAR(255),
+    estado VARCHAR(255),
+    cep VARCHAR(20),
+    tipoUsuario VARCHAR(50),
+    admin BOOLEAN
+);
