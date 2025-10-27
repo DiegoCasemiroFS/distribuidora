100af5d62a549efe11ce0ba658adee0752daaa1f
diego-casemiro <dcfs1998@gmail.com>
2025-10-27 19:18:40 -0300

    feat: implement product creation with duplicate name check and exception handling

--- src/main/java/br/com/DiegoCasemiroFS/distribuidora/service/ProductService.java
+++ src/main/java/br/com/DiegoCasemiroFS/distribuidora/service/ProductService.java
@@ -4,6 +4,7 @@ import br.com.DiegoCasemiroFS.distribuidora.entity.product.CreateRequestDto;
 import br.com.DiegoCasemiroFS.distribuidora.entity.product.PriceRequestDto;
 import br.com.DiegoCasemiroFS.distribuidora.entity.product.Product;
 import br.com.DiegoCasemiroFS.distribuidora.entity.product.StockRequestDto;
+import br.com.DiegoCasemiroFS.distribuidora.exception.ProductAlreadyExistsException;
 import br.com.DiegoCasemiroFS.distribuidora.exception.ProductNotFoundException;
 import br.com.DiegoCasemiroFS.distribuidora.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,14 +28,18 @@ public class ProductService {
     }
 
     public Product createProduct(CreateRequestDto createRequestDto){
-        Product product = new Product();
 
-        product.setName(createRequestDto.getName());
-        product.setProductType(createRequestDto.getProductType());
-        product.setPrice(createRequestDto.getPrice());
-        product.setStock(createRequestDto.getQuantity());
+        if(!productRepository.existsByNameIgnoreCase(createRequestDto.getName())){
+            Product product = new Product();
 
-        return productRepository.save(product);
+            product.setName(createRequestDto.getName());
+            product.setProductType(createRequestDto.getProductType());
+            product.setPrice(createRequestDto.getPrice());
+            product.setStock(createRequestDto.getQuantity());
+
+            return productRepository.save(product);
+        }
+        throw new ProductAlreadyExistsException();
     }
 
     public Product updatePrice(Long id, PriceRequestDto priceRequestDto) {
